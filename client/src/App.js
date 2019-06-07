3a40d833b1b78504560f54cf951d332dbd419c65
francois <desobry.f@gmail.com>
2019-06-07 13:52:06 +0200

    test hooks with add notes get nodes and delete notes

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,61 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const [note, setNote] = useState([])
-  const [input, setInput] = useState('')
+  const [notes, setNotes] = useState([])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [note, setNote] = useState({})
+
+  // componentDidmount / didUpdate
+  useEffect(()=>{
+    const fetchData = async () => {
+      const data = await fetch('/api/notes')
+      const fetchNotes = await data.json()
+      setNotes(fetchNotes)
+    }
+    fetchData()
+  }, [note])
+  
+  // I don't know if it s good practice to do that
+  const addNote = async (e) => {
+    e.preventDefault()
+    console.log('trigger')
+    const data = await fetch('api/notes', {
+      method:"POST",
+      headers:{'Content-Type': 'application/json'},
+      body: JSON.stringify({'title':title, 'body':body})
+    })
+    const fetchNewNotes = data.json()
+    setNote(fetchNewNotes)
+  }
+
+  const delNote = async (id) => {
+    const data = await fetch(`api/notes/${id}`, {
+      method:"DELETE",
+      headers:{'Content-Type': 'application/json'}
+    })
+    const isSuccess = data.json()
+    return !isSuccess ? null : setNote({})
+  }
+
   return (
     <div className="App">
       <h1>Hello</h1>
-     <form onSubmit={(e) =>{
-        e.preventDefault()
-        setNote([...note, input])}}>
-       <input onChange={e => setInput(e.target.value)}/>
-     </form>
-     {note.map(n => {
+      <form onSubmit={addNote}>
+        <input placeholder='title' onChange={e=> 
+          setTitle(e.target.value)}/>
+        <input placeholder='body' onChange={e=> setBody(e.target.value)}/>
+        <input type='submit' />
+      </form>
+     {notes.map(note => {
+       let id = note._id
        return(
-         <p>{n}</p>
+         <div key={id}>
+          <h2>{note.title}</h2>
+          <p>{note.body}</p>
+          <button onClick={() => delNote(id)}>X</button>
+         </div>
        )
      })}
     </div>
