437bb05ce16e31d4f17457b4b8e352aca21bab87
francois <desobry.f@gmail.com>
2019-06-11 19:21:34 +0200

    fix notes routes/api

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -2,29 +2,23 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../../middleware/auth')
 
-const router = express.Router();
 
 const Note = require('../../models/Note');
 
-router.post('/', auth, (req, res)=>{
-    const newNote = new Note({
-        title: req.body.title,
-        body: req.body.body
-    })
+router.get('/', (req, res)=>{
+  Note.find()
+  .then(notes => res.json(notes))
+})
 
-router.post('/', (req, res) => {
+router.post('/', auth, (req, res) => {
   const newNote = new Note({
     title: req.body.title,
     body: req.body.body,
   });
-
-router.delete('/:id', auth, (req,res)=>{
-    Note.findById(req.params.id)
-    .then(note => note.remove().then(()=> res.json({success :true})))
-    .catch(err => res.status(400).json({success:false}))
+  newNote.save().then(note=>res.json(note))
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', auth, (req, res) => {
   Note.findById(req.params.id)
     .then(note => note.remove().then(() => res.json({ success: true })))
     .catch(err => res.status(400).json({ success: false }));
