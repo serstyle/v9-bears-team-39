694b1fcbd76b8699b6f0f0062d875c0770dce2ee
francois <desobry.f@gmail.com>
2019-06-12 09:59:21 +0200

    homepage mockup + folder structure

--- client/src/views/HomePage/HomePage.js
+++ client/src/views/HomePage/HomePage.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 // style
-
 import Container from '@material-ui/core/Container';
+
+// component
+import Navbar from '../../components/Navbar';
 
 export default function HomePage() {
   return (
@@ -11,7 +13,23 @@ export default function HomePage() {
       <Helmet>
         <title>Home</title>
       </Helmet>
-      <Container>Hello</Container>
+      <Navbar />
+      <Container
+        style={{ paddingTop: '20px', background: '#F5F5F5', height: '100vh' }}
+      >
+        <p style={{ textAlign: 'center' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+          maximus, nisi eget rutrum dapibus, tellus orci efficitur urna, tempor
+          tempus elit diam at lorem. Quisque imperdiet ut tortor vitae blandit.
+          Nulla sed lacus nec tortor gravida aliquet ut a odio. Mauris et massa
+          vitae dolor condimentum bibendum. Maecenas accumsan enim lacinia arcu
+          ornare, sed tempus leo vestibulum. Donec at nunc at nunc convallis
+          finibus non sed eros. Pellentesque vehicula mattis mauris, vitae
+          aliquet urna vestibulum auctor. Duis leo libero, ultrices vel interdum
+          ut, commodo et ligula. Sed a aliquam mi. Sed auctor eget ipsum sit
+          amet mollis.
+        </p>
+      </Container>
     </div>
   );
 }
