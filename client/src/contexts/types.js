c52816c33eb64fd2afdb111ac1222238e0723c5c
Fabian Starke <fabian@starke.fr>
2019-06-21 08:49:50 +0200

    Add Alerts and fix in auth route

--- client/src/contexts/types.js
+++ client/src/contexts/types.js
@@ -1,6 +1,13 @@
 // Registration
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
 // Auth
 export const AUTH_ERROR = 'AUTH_ERROR';
+
+// Alert
+
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
