de16925b7b26e8d1c09c269e53bdd4d3f7dc56d2
francois <desobry.f@gmail.com>
2019-07-20 17:40:17 +0200

    Add note and delete note

--- client/src/components/MyNotesForm.js
+++ client/src/components/MyNotesForm.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import FormTextField from './FormTextField';
+import FormButton from './FormButton';
 
 export default function MyNotesForm({ addNote }) {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+  const submitAddNote = e => {
+    addNote(e, title, body);
+  };
   return (
     <div>
       <h2>Add a note</h2>
-      <form onSubmit={e => addNote(e, title, body)}>
+      <form onSubmit={submitAddNote}>
         <FormTextField
           id="note-title"
           label="Title"
@@ -24,7 +28,9 @@ export default function MyNotesForm({ addNote }) {
           value={body}
           onChange={e => setBody(e.target.value)}
         />
-        <button type="submit">Submit</button>
+        <FormButton name="Save" onSubmit={submitAddNote}>
+          Submit
+        </FormButton>
       </form>
     </div>
   );
